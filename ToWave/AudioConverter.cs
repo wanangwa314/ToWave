796d6b74874b5d15beb9f290fdbf7f34e161190a
Martin-wanangwa-nyirenda <martinnyirenda8@gmail.com>
2019-02-18 22:53:46 +0200

    Created AudioConverter class and edited the UI

---
+++ ToWave/AudioConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using NAudio.Wave;
+using System.IO;
+
+
+namespace ToWave
+{
+
+    class AudioConverter
+    {
+        public string OutPutFile { get; set; }
+        public string Filename { get; set; }
+
+        public AudioConverter(string filename, string outFile)
+        {
+            OutPutFile = outFile;
+            Filename = filename;
+        }
+
+        public void Convert()
+        {
+            using (AudioFileReader reader = new AudioFileReader(Filename))
+            using (WaveFileWriter writer = new WaveFileWriter(OutPutFile, reader.WaveFormat))
+            {
+                reader.CopyTo(writer);
+            }
+        }
+    }
+}
--- ToWave/Form1.Designer.cs
+++ ToWave/Form1.Designer.cs
@@ -28,13 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.selectFileBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.selectFolderBtn = new System.Windows.Forms.Button();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.filenameLabel = new System.Windows.Forms.Label();
+            this.filepathLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // selectFileBtn
+            // 
+            this.selectFileBtn.Location = new System.Drawing.Point(68, 30);
+            this.selectFileBtn.Name = "selectFileBtn";
+            this.selectFileBtn.Size = new System.Drawing.Size(257, 57);
+            this.selectFileBtn.TabIndex = 0;
+            this.selectFileBtn.Text = "Select File To Convert";
+            this.selectFileBtn.UseVisualStyleBackColor = true;
+            this.selectFileBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // selectFolderBtn
+            // 
+            this.selectFolderBtn.Location = new System.Drawing.Point(378, 197);
+            this.selectFolderBtn.Name = "selectFolderBtn";
+            this.selectFolderBtn.Size = new System.Drawing.Size(70, 22);
+            this.selectFolderBtn.TabIndex = 1;
+            this.selectFolderBtn.Text = "Browse";
+            this.selectFolderBtn.UseVisualStyleBackColor = true;
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(46, 151);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(326, 20);
+            this.fileNameTextBox.TabIndex = 2;
+            this.fileNameTextBox.Text = "Converted.wav";
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(46, 199);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(326, 20);
+            this.filePathTextBox.TabIndex = 3;
+            // 
+            // filenameLabel
+            // 
+            this.filenameLabel.AutoSize = true;
+            this.filenameLabel.Location = new System.Drawing.Point(46, 132);
+            this.filenameLabel.Name = "filenameLabel";
+            this.filenameLabel.Size = new System.Drawing.Size(57, 13);
+            this.filenameLabel.TabIndex = 4;
+            this.filenameLabel.Text = "File Name:";
+            // 
+            // filepathLabel
+            // 
+            this.filepathLabel.AutoSize = true;
+            this.filepathLabel.Location = new System.Drawing.Point(46, 178);
+            this.filepathLabel.Name = "filepathLabel";
+            this.filepathLabel.Size = new System.Drawing.Size(51, 13);
+            this.filepathLabel.TabIndex = 5;
+            this.filepathLabel.Text = "File Path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(514, 262);
+            this.Controls.Add(this.filepathLabel);
+            this.Controls.Add(this.filenameLabel);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.selectFolderBtn);
+            this.Controls.Add(this.selectFileBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button selectFileBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button selectFolderBtn;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Label filenameLabel;
+        private System.Windows.Forms.Label filepathLabel;
     }
 }
 
